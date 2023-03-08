54c517f20ce0602af1ecff9f5c99509fa11ad409
skhushi26 <khushaliketanshah@gmail.com>
2023-03-08 11:36:35 -0500

    update image path

--- src/ProductView.js
+++ src/ProductView.js
@@ -4,15 +4,13 @@ import ThreeSixty from "react-360-view";
 function ProductView() {
   return (
     <div>
-      <div style={{ width: "40%", margin: "30px", position: "relative" }}>
-        <ThreeSixty
-          amount={36}
-          imagePath="https://scaleflex.cloudimg.io/v7/demo/vivo-mobile"
-          fileName="product-{index}.jpg"
-          autoplay
-          loop
-        />
-      </div>
+      <ThreeSixty
+        amount={251}
+        imagePath="https://mining-now.s3.us-east-2.amazonaws.com/prod/images/miner/360/z-15"
+        fileName="z-15-{index}.png"
+        autoplay
+        loop
+      />
     </div>
   );
 }
